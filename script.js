1f1f13c3cfd38d036c288a3190c5b4c99a274a2d
Saba Farahani <sabafarahani5@gmail.com>
2022-06-09 00:09:23 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const weather = {
+  apiKey: "36dd6b01bf2ad4c882fa308ac2da075b",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((resp) => resp.json())
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    //console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+
+  search: function () {
+    const city = document.querySelector(".search").value;
+    this.fetchWeather(city);
+  },
+};
+
+document.querySelector(".btn").addEventListener("click", () => {
+  weather.search();
+});
+
+document.querySelector(".search").addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    weather.search();
+  }
+});
